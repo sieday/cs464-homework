762930def90e164e3d46ce40719838951acbd255
Sienna Day <54122405+sieday@users.noreply.github.com>
2023-10-06 11:53:07 -0700

    Update to follow AirBnb Style Guide, remove unnecessary else statements, remove unnecessary returns

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,43 +1,34 @@
 const elem = document.querySelector('input');
-elem.addEventListener('input', handleInput);
+elem.addEventListener('input', palindromeCheck);
 const output = document.querySelector('.output');
 
-function handleInput() {
-  const user_input = elem.value;
-  return palindrome_check(user_input);
-}
-
-function palindrome_check(user_input) {
-  if (user_input < 0) {
-    return input_err();
-  } else if (user_input === '') {
-    return input_err();
-  } else if (
-    user_input.toString() === user_input.toString().split('').reverse().join('')
-  ) {
-    return pali();
-  } else {
-    return non_pali();
+function palindromeCheck() {
+  const userInput = elem.value;
+  if (userInput < 0) {
+    return inputError();
   }
+  if (
+    userInput.toString() === userInput.toString().split('').reverse().join('')
+  ) {
+    return palindrome();
+  }
+  return notPalindrome();
 }
 
-function non_pali() {
+function notPalindrome() {
   output.style.color = 'red';
   output.innerHTML = 'No. Try Again';
-  console.log('False');
   return;
 }
 
-function pali() {
+function palindrome() {
   output.style.color = 'green';
   output.innerHTML = 'Yes. This is a palindrome!';
-  console.log('True');
   return;
 }
 
-function input_err() {
+function inputError() {
   output.style.color = 'red';
   output.innerHTML = 'Error. Please enter a positive number.';
-  console.log('False');
-  return false;
+  return;
 }
