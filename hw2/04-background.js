204c714fda1fa16a114db6cbc07f8c377c0ce96d
Sienna Day <54122405+sieday@users.noreply.github.com>
2023-10-18 15:40:26 -0700

    Add Count, Background, and Unfinished Form

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,42 @@
-// Add your code here
+var intervalID = setInterval(newColor, 3000);
+const time = document.querySelector('#time');
+const startButton = document.querySelector('#start');
+startButton.addEventListener('click', begin);
+addEventListener('load', swapInterval);
+
+function begin() {
+  startButton.removeEventListener('click', startButton);
+  startButton.addEventListener('click', stop);
+  startButton.innerHTML = 'Stop';
+  startButton.className = 'mt-4 btn btn-danger';
+  swapInterval();
+}
+
+function stop() {
+  startButton.removeEventListener('click', stop);
+  startButton.addEventListener('click', startButton);
+  startButton.innerHTML = 'Start';
+  startButton.className = 'mt-4 btn btn-primary';
+  clearInterval(intervalID);
+}
+
+function swapInterval() {
+  clearInterval(intervalID);
+  changeBackground();
+}
+
+function changeBackground() {
+  userInput = time.value;
+  var interval = 3;
+  if (userInput === '') {
+    interval = 3;
+  } else {
+    interval = userInput;
+  }
+  intervalID = setInterval(newColor, interval * 1000);
+}
+
+function newColor() {
+  const randColor = '#' + Math.floor(Math.random() * 12345678).toString(16);
+  document.body.style.background = randColor;
+}
